0f6755498ee66fc5b24e558340c58fa207d6e84c
pagebook1 <47450220+pagebook1@users.noreply.github.com>
2023-09-15 04:24:23 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,19 +1,92 @@
 // Create a web server
 
-// Import module
+// Import modules
 const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const mongoose = require('mongoose');
 
-// Handle request and response
-router.get('/', commentController.comment_list);
-router.get('/create', commentController.comment_create_get);
-router.post('/create', commentController.comment_create_post);
-router.get('/:id/delete', commentController.comment_delete_get);
-router.post('/:id/delete', commentController.comment_delete_post);
-router.get('/:id/update', commentController.comment_update_get);
-router.post('/:id/update', commentController.comment_update_post);
-router.get('/:id', commentController.comment_detail);
+// Import models
+const Comment = require('./models/comment');
 
-// Export module
-module.exports = router;
+// Create express app
+const app = express();
+
+// Configure app
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+// Connect to database
+mongoose.connect('mongodb://localhost:27017/comments');
+
+// Create endpoints
+app.get('/comments', (req, res) => {
+  Comment.find()
+    .then((comments) => {
+      res.json(comments);
+    })
+    .catch((err) => {
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.get('/comments/:id', (req, res) => {
+  Comment.findById(req.params.id)
+    .then((comment) => {
+      res.json(comment);
+    })
+    .catch((err) => {
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.post('/comments', (req, res) => {
+  const comment = new Comment({
+    name: req.body.name,
+    comment: req.body.comment,
+  });
+
+  comment
+    .save()
+    .then(() => {
+      res.send('Comment successfully added');
+    })
+    .catch((err) => {
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.put('/comments/:id', (req, res) => {
+  Comment.findById(req.params.id)
+    .then((comment) => {
+      comment.name = req.body.name;
+      comment.comment = req.body.comment;
+      comment
+        .save()
+        .then(() => {
+          res.send('Comment successfully updated');
+        })
+        .catch((err) => {
+          res.status(500).send('Error: ' + err);
+        });
+    })
+    .catch((err) => {
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+app.delete('/comments/:id', (req, res) => {
+  Comment.findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.send('Comment successfully deleted');
+    })
+    .catch((err) => {
+      res.status(500).send('Error: ' + err);
+    });
+});
+
+// Start listening
+app.listen(3000, () => {
+  console.log('Server started at http://localhost:3000');
+});
